ec11d3b3e284388e9120e3f1db19d2f7249f5b9a
juankmp <juankmp@gmail.com>
2020-05-06 19:44:00 +0200

    Bonus Started!

---
+++ extra-work/m1/lab-bash-master/bonus.sh
@@ -0,0 +1,23 @@
+#Este es el código del bonus del lab-bash-master
+
+echo ------------------------------------
+echo 'Este es el bonus del ejercicio'
+name='Juan Munoz'
+
+echo 'Mi nombre es:' $name
+
+echo 'Directorio' $name 'creado' 
+mkdir $name
+echo 'Directorio' $name 'borrado'
+rm -rf $name
+cd lorem/
+
+
+for i in $( ls ); do
+	echo $i
+	count = -n $i | wc -c
+	echo "$i has $count characthers length:"
+	echo -n $i | wc -c
+	echo '--- Next File:'
+done  
+
