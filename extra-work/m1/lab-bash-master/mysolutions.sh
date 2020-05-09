b813c40633f07313f8b7bc0df74366fc905e8b31
Dav3whit3 <davidblancoferrandez@gmail.com>
2020-05-09 09:14:18 +0200

    update

---
+++ extra-work/m1/lab-bash-master/mysolutions.sh
@@ -0,0 +1,11 @@
+
+#Print Hello World in console.
+echo "Hellow World"
+
+#Create a new folder named new_dir
+mkdir new_dir
+
+#Delete new_dir folder
+rmdir new_dir
+
+#
--- extra-work/m1/lab-bash-master/prueba.sh
+++ extra-work/m1/lab-bash-master/prueba.sh
@@ -1,1 +1,2 @@
-mkdir new_dir
+#/usr/bin/bash
+cd /home/david/Documents/dataptmad0420/extra-work/m1/lab-bash-master/lorem
