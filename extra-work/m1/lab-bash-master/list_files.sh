05de7216de2a71c066797e25dd232607a49d744f
Laserdan <vlj993@gmail.com>
2020-05-10 13:16:34 +0200

    lab-finished

--- extra-work/m1/lab-bash-master/list_files.sh
+++ extra-work/m1/lab-bash-master/list_files.sh
@@ -8,3 +8,15 @@ ls
 rm -r $name
 
 cd ~/Documentos/ironhack_repos/dataptmad0420/extra-work/m1/lab-bash-master/lorem
+for file in *
+do
+	echo $file
+	expr length $file
+done
+
+for file in *
+do
+	echo $file has $(expr length $file) characters lenght
+done
+
+
