3da4b07f12187ee1fdfee3d2c989a51ddb19d36d
IvanCompany <ivancompany92@gmail.com>
2020-05-10 22:34:48 +0200

    continue with Bonus

--- extra-work/m1/lab-bash-master/list_files.sh
+++ extra-work/m1/lab-bash-master/list_files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash.
+ls
+
 #Load your name in the variable name:
 echo your name is:
 read name
@@ -8,21 +10,41 @@ echo  $name
 
 #Create a folder named after the variable name:
 mkdir $name
+echo
 
 #Delete thar folder:
 rmdir $name
+echo
 
 #For every folder in lorem print the total number of characters of every file name. Try using a for loop to show every file name.
 #Print every file:
 for i in lorem/*
 do
-printf "$i"
-echo 
-
+printf "$i\n"
 done
+echo
 
 #Print the number of characters of every file name:
-for k in lorem/*
+for k in  lorem/*
 do
-printf cat $k | wc -m 
+cat $k|wc -m
+
 done
+echo
+
+#Print the number of characters of every file name with the following format: lorem has 5 characters lenght
+for M in  lorem/*
+do
+echo  $M has $(cat $M|wc -m)  characters lenght
+
+done
+echo
+
+#Show the system processes being executed (show them hierarchically):
+top
+echo
+
+ps -axrj
+echo
+
+
