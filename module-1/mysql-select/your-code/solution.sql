3c98f3e548cdeef1065e098604455e37bcc374d9
Melonn96 <diegomelon1996@gmail.com>
2020-05-17 15:33:03 +0200

    Second Challenge Complete

--- module-1/mysql-select/your-code/solution.sql
+++ module-1/mysql-select/your-code/solution.sql
@@ -13,4 +13,22 @@ ON titles.title_id = titleauthor.title_id
 JOIN authors 
 ON titleauthor .au_id = authors.au_id;
 
- 
+--Challenge 2 - Who Have Published How Many At Where?
+SELECT
+authors.au_id as "AUTHOR ID",
+authors.au_lname as "LAST NAME",
+authors.au_fname as "FIRST NAME",
+publishers.pub_name as PUBLISHER,
+count(titles.pub_id) as "TITLE COUNT"
+FROM titles 
+JOIN publishers 
+ON titles.pub_id = publishers.pub_id 
+JOIN titleauthor 
+ON titles.title_id = titleauthor.title_id 
+JOIN authors 
+ON titleauthor .au_id = authors.au_id
+GROUP BY "AUTHOR ID",
+"LAST NAME",
+"FIRST NAME",
+PUBLISHER
+ORDER BY "TITLE COUNT" DESC; 
