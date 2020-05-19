07b63bf2f07bc374425baaf8aee3ef744d034f90
ChristianJavierMelo <christian.javier.melo@gmail.com>
2020-05-19 17:08:29 +0200

    first challenge completed

--- module-1/mysql-select/your-code/solution.sql
+++ module-1/mysql-select/your-code/solution.sql
@@ -1,1 +1,19 @@
 Solutions according to the lab
+
+1st challenge:
+
+SELECT 
+authors.au_id as AUTHOR_ID, 
+authors.au_lname as LAST_NAME, 
+authors.au_fname as FIRST_NAME, 
+titles.title as TITLE, 
+publishers.pub_name as PUBLISHER
+from titles
+join titleauthor on titleauthor.title_id = titles.title_id
+join authors on authors.au_id = titleauthor.au_id
+join publishers on publishers.pub_id = titles.pub_id;
+
+SELECT count(*)
+from titleauthor;
+
+
