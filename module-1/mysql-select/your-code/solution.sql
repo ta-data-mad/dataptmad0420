7c828fec20acfb25f61dae653ed67cfe13c1e339
Melonn96 <diegomelon1996@gmail.com>
2020-05-20 19:10:20 +0200

    lab-finished

--- module-1/mysql-select/your-code/solution.sql
+++ module-1/mysql-select/your-code/solution.sql
@@ -32,3 +32,34 @@ GROUP BY "AUTHOR ID",
 "FIRST NAME",
 PUBLISHER
 ORDER BY "TITLE COUNT" DESC; 
+
+--Challenge 3 - Best Selling Authors
+SELECT
+authors.au_id as "AUTHOR ID",
+authors.au_lname as "LAST NAME",
+authors.au_fname as "FIRST NAME",
+sum(ytd_sales) as TOTAL
+FROM titles 
+JOIN publishers 
+ON titles.pub_id = publishers.pub_id 
+JOIN titleauthor 
+ON titles.title_id = titleauthor.title_id 
+JOIN authors 
+ON titleauthor .au_id = authors.au_id
+GROUP BY authors.au_id, authors.au_lname, authors.au_lname
+ORDER BY TOTAL DESC
+LIMIT 3;
+
+--Challenge 4 - Best Sellers Authors Ranking
+SELECT
+authors.au_id as "AUTHOR ID",
+authors.au_lname as "LAST NAME",
+authors.au_fname as "FIRST NAME",
+COALESCE(sum(ytd_sales),0) as TOTAL
+FROM authors
+LEFT JOIN titleauthor 
+ON authors.au_id = titleauthor.au_id 
+LEFT JOIN titles 
+ON titleauthor.title_id = titles.title_id 
+GROUP BY authors.au_id
+ORDER BY TOTAL DESC;
