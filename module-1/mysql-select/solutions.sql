04d2e5ddc3deea3b376969b70b773e91350bf9c5
IvanCompany <ivancompany92@gmail.com>
2020-05-21 19:46:04 +0200

    challenge 3 finished

--- module-1/mysql-select/solutions.sql
+++ module-1/mysql-select/solutions.sql
@@ -1,1 +1,48 @@
-Challenge 1
+--Challenge 1:
+SELECT 
+authors.au_id as Author_ID,
+authors.au_lname as Last_Name,
+authors.au_fname as First_Name,
+titles.title as Title_Count,
+publishers.pub_name as Publisher
+from titles
+join titleauthor 
+on titles.title_id = titleauthor.title_id
+join authors 
+on titleauthor.au_id = authors.au_id 
+join publishers 
+on publishers.pub_id=titles.pub_id;
+ 
+--Challenge 2:
+SELECT 
+authors.au_id as Author_ID,
+authors.au_lname as Last_Name,
+authors.au_fname as First_Name,
+publishers.pub_name as Publisher,
+count (titles.title_id ) as Tittle_count -- we count the titles with the conditions "group by"
+from titles
+join titleauthor 
+on titles.title_id = titleauthor.title_id
+join authors 
+on titleauthor.au_id = authors.au_id 
+join publishers 
+on publishers.pub_id=titles.pub_id
+GROUP BY authors.au_id, publishers.pub_id  -- we put 2 conditions for agroup the list
+ORDER BY authors.au_id DESC;
+
+--challenge 3
+select 
+authors.au_id as Author_ID,
+authors.au_lname as Last_Name,
+authors.au_fname as First_Name,
+sales.qty  as Total
+from titles
+join titleauthor 
+on titles.title_id = titleauthor.title_id
+join authors 
+on titleauthor.au_id = authors.au_id 
+join sales 
+on sales.title_id =titles.title_id 
+group by sales.title_id 
+ORDER by qty desc
+limit 3;
