87271d07f942721bcaa5f768804e9a12a797f28e
Sandra <sandra@sandras-macbook-pro.home>
2020-05-21 23:02:38 +0200

    added first challenge

--- module-1/mysql-select/your-code/solutions.sql
+++ module-1/mysql-select/your-code/solutions.sql
@@ -1,1 +1,19 @@
-lab sql
+CHALLENGE 1:
+select authors.au_id as author_id, 
+authors.au_lname as last_name,
+authors.au_fname as first_name,
+titles.title as title,
+publishers.pub_name as publisher
+from titles
+join titleauthor
+on titleauthor.title_id = titles.title_id
+join authors 
+on authors.au_id = titleauthor.au_id
+join publishers
+on publishers.pub_id = titles.pub_id
+
+CHALLENGE 2:
+
+CHALLENGE 3:
+
+CHALLENGE 4:
