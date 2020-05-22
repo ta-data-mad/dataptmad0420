6e388bc24d7aa0300d097cc3b97aa07f8bca6953
Marta <martaperezpuerta@gmail.com>
2020-05-22 02:31:28 +0200

    all challenges solved, pending to review

--- module-1/mysql-select/solutions.sql
+++ module-1/mysql-select/solutions.sql
@@ -39,4 +39,37 @@ author_id desc
 
 CHALLENGE 3
 
+select
+authors.au_id as author_id,
+authors.au_fname as first_name,
+authors.au_lname as last_name,
+count(sales.qty) as total
+from titles
+join titleauthor
+on titleauthor.title_id = titles.title_id
+join authors
+on authors.au_id = titleauthor.au_id
+join sales
+on sales.title_id = titles.title_id
+group by authors.au_id
+order by total desc
+limit 3
 
+
+CHALLENGE 4
+
+
+select
+authors.au_id as author_id,
+authors.au_fname as first_name,
+authors.au_lname as last_name,
+count(sales.qty) as total
+from authors
+left join titleauthor
+on authors.au_id = titleauthor.au_id
+left join titles
+on titleauthor.title_id = titles.title_id
+join sales
+on sales.title_id = titles.title_id
+group by authors.au_id
+order by total desc
