5293ac5c26ca82f61adba3e9ebd3c5c6af345126
Sandra <sandra@sandras-macbook-pro.home>
2020-05-23 21:00:18 +0200

    lab-finished

--- module-1/mysql-select/your-code/solutions.sql
+++ module-1/mysql-select/your-code/solutions.sql
@@ -1,19 +1,61 @@
-CHALLENGE 1:
-select authors.au_id as author_id, 
-authors.au_lname as last_name,
-authors.au_fname as first_name,
-titles.title as title,
-publishers.pub_name as publisher
-from titles
+------- CHALLENGE 1 -------
+SELECT 
+authors.au_id as "AUTHOR ID", 
+authors.au_lname AS "LAST NAME", 
+authors.au_fname AS "FIRST NAME", 
+titles.title AS "TITLE", 
+publishers.pub_name AS "PUBLISHER"
+FROM titles
 join titleauthor
 on titleauthor.title_id = titles.title_id
 join authors 
-on authors.au_id = titleauthor.au_id
-join publishers
-on publishers.pub_id = titles.pub_id
+on authors.au_id = titleauthor.au_id 
+join publishers 
+on titles.pub_id = publishers.pub_id 
 
-CHALLENGE 2:
+------- CHALLENGE 2 -------
+SELECT 
+authors.au_id as "AUTHOR ID", 
+authors.au_lname AS "LAST NAME", 
+authors.au_fname AS "FIRST NAME", 
+publishers.pub_name AS "PUBLISHER",
+count (titles.pub_id) as "TITLE COUNT"
+FROM titles 
+join titleauthor
+on titleauthor.title_id = titles.title_id
+join authors 
+on authors.au_id = titleauthor.au_id 
+join publishers 
+on titles.pub_id = publishers.pub_id 
+GROUP BY 1,2,3,4
 
-CHALLENGE 3:
+------- CHALLENGE 3 -------
+SELECT 
+authors.au_id as "AUTHOR ID", 
+authors.au_lname AS "LAST NAME", 
+authors.au_fname AS "FIRST NAME", 
+sum(sales.qty)
+FROM authors 
+join titleauthor 
+on titleauthor.au_id = authors.au_id 
+join titles 
+on titles.title_id = titleauthor.title_id 
+join sales 
+on sales.title_id = titleauthor.title_id
+group by 1,2,3
+order by 4 DESC 
+limit 3
 
-CHALLENGE 4:
+------- CHALLENGE 4 -------
+SELECT 
+authors.au_id as "AUTHOR ID",
+authors.au_lname as "LAST NAME",
+authors.au_fname as "FIRST NAME",
+COALESCE(SUM(sales.qty),case when sales.qty ISNULL THEN 0 END) as "TOTAL"
+FROM authors
+JOIN titleauthor
+on titleauthor.au_id = authors.au_id
+join sales
+on sales.title_id = titleauthor.title_id
+group BY titleauthor.au_id
+order BY total DESC;
