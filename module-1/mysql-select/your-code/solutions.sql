33f83afc8d8a573e1bba47f07b4526681577e91c
bfmilan <bfmilan@gmail.com>
2020-05-23 20:10:11 +0200

    lab-finished

--- module-1/mysql-select/your-code/solutions.sql
+++ module-1/mysql-select/your-code/solutions.sql
@@ -1,10 +1,20 @@
+/*Challenge 1 - Who Have Published What At Where?
+In this challenge you will write a MySQL SELECT query that joins various tables to figure out what titles each author 
+has published at which publishers. Your output should have at least the following columns:
+
+    AUTHOR ID - the ID of the author
+    LAST NAME - author last name
+    FIRST NAME - author first name
+    TITLE - name of the published title
+    PUBLISHER - name of the publisher where the title was published
+*/
 
 SELECT 
-authors.au_id as author_id, 
-authors.au_lname as last_name, 
-authors.au_fname as first_name,
-titles.title as title, 
-publishers.pub_name as publisher
+authors.au_id as 'AUTHOR ID', 
+authors.au_lname as 'LAST NAME', 
+authors.au_fname as 'FIRST NAME',
+titles.title as 'TITLE', 
+publishers.pub_name as 'PUBLISHER'
 from titles
 join titleauthor 
 on titleauthor.title_id = titles.title_id
@@ -13,172 +23,97 @@ on authors.au_id = titleauthor.au_id
 join publishers
 on publishers.pub_id = titles.pub_id; 
 
-SELECT count(*)
-FROM  titleauthor
+/*
+Challenge 2 - Who Have Published How Many At Where?
+Elevating from your solution in Challenge 1, query how many titles each author 
+has published at each publisher. Your output should look something like below:
 
+Challenge 2 output
+*/
 
-
--- 
-    AUTHOR ID - the ID of the author
-    LAST NAME - author last name
-    FIRST NAME - author first name
-    TITLE - name of the published title
-    PUBLISHER - name of the publisher where the title was published
-
-
-
-
-SELECT title, type, price, 
-sum(qty) as units_sold
+SELECT 
+authors.au_id as 'AUTHOR ID', 
+authors.au_lname as 'LAST NAME', 
+authors.au_fname as 'FIRST NAME',
+publishers.pub_name as 'PUBLISHER',
+count(titleauthor.title_id) as 'TITLE COUNT'
 from titles
-join sales 
-on sales.title_id = titles.title_id 
-group by title, type, price
+join titleauthor 
+on titleauthor.title_id = titles.title_id
+join authors 
+on authors.au_id = titleauthor.au_id 
+join publishers
+on publishers.pub_id = titles.pub_id
+group by authors.au_id
+ORDER BY authors.au_id DESC; 
 
--- algun titulo no tiene ningun pedido o venta asocisada, entonces se pierde en el left join
--- LEFT JOIN para tener todos titulos en query 
-SELECT title, type, price, 
-sum(qty) as units_sold
-from titles
-left join sales 
-on sales.title_id = titles.title_id 
-group by title, type, price
+SELECT count(titleauthor.title_id) as 'TITLE COUNT'
+from titleauthor 
+-- Output = 25
 
--- RIGHT right join 
-SELECT title , type, price, 
-sum(qty) as units_sold
-from titles
-left join sales
-on sales.title_id = titles.title_id 
-group by title, type, price; 
+SELECT count(title_id) as 'TITLE COUNT'
+from titleauthor 
+-- Output = 25 
 
 
---EMPLOYEE
--- emploeados no asiciados a job y jobs no asociados a ningun empleado 
--- ver las tablas que quiero merge
-select * from employee; 
-select * from jobs; 
--- venga esa query 
 
-select * from employee 
-join jobs on jobs.job_id = employee.job_id; 
+/*
+Challenge 3 - Best Selling Authors
+Who are the top 3 authors who have sold the highest number of titles? 
+Write a query to find out.
 
--- OUTERJOIN pero me falta los empleados sin job id y los trabajos sin empleados 
-select * from employee 
-left join jobs on jobs.job_id = employee.job_id; 
---
-SELECT employee.fname,  employee.hire_date,  jobs.job_desc,  jobs.job_id
-FROM jobs
-LEFT JOIN employee
-on jobs.job_id = employee.job_id
-UNION
-SELECT employee.fname,  employee.hire_date,  jobs.job_desc,  jobs.job_id
-FROM employee
-LEFT JOIN jobs
-on jobs.job_id = employee.job_id;
+Requirements:
+    Your output should have the following columns:
+        AUTHOR ID - the ID of the author
+        LAST NAME - author last name
+        FIRST NAME - author first name
+        TOTAL - total number of titles sold from this author
+    Your output should be ordered based on TOTAL from high to low.
+    Only output the top 3 best selling authors.
 
-
--- combined queries (WITH) 
-select ventas
-from (select title, type, 
-ytd_sales as ventas
-from titles); 
-
-
--- un fig 
-select AVG(ventas) 
-from (select title, type, 
-ytd_sales as ventas
-from titles); 
-
--- dar nombre a consulta 'employees-custom' para que cree una tabla 
-WITH 
-employees_custom AS 
-(
-SELECT *
-FROM employee
-),
-jobs_custom AS
-(
-SELECT *
-FROM jobs
-JOIN employees_custom
-ON employees_custom.job_id = jobs.job_id
-)
-SELECT * from jobs_custom;
-
+Challenge 3 output
+*/
 
 
 SELECT 
-pub_name,
-count(titles.title_id) as titles_published
-FROM titles
-left join publishers 
-ON publishers.pub_id = titles.pub_id
-GROUP BY pub_name
-ORDER BY titles_published DESC;
-
-
--- darle la vuelta a la tabla 
--- Some commandds 
-
--- para seleccionar todo 
-SELECT * 
--- Alias de titles - t 
-FROM titles
--- Sacar categorias - repetidas por cada libro - ueremos solo una categoria por fila 
-SELECT DISTINCT type
-FROM titles t
-SELECT *
-FROM sales 
-
--- codigo y columna que sea lo que han vendido en total x unidades de ese titulo' 
-select title_id, sum(qty)
-from sales 
-group by title_id 
-
--- codigo y columna que sea lo que han vendido en media x unidades de ese titulo' 
-select title_id, AVG(qty) as avg_ventas
-from sales 
-group by title_id 
-
--- Aggregation forms: SUM, AVG. MAX, MIN, MED,   
-select title_id, AVG(qty) as avg_ventas
-from sales 
-group by title_id 
+authors.au_id as 'AUTHOR ID', 
+authors.au_lname as 'LAST NAME', 
+authors.au_fname as 'FIRST NAME',
+sum(sales.qty) as 'TOTAL'
+from titles
+INNER join titleauthor
+on titleauthor.title_id = titles.title_id
+INNER join authors
+on authors.au_id = titleauthor.au_id 
+INNER join sales 
+on sales.title_id = titles.title_id
+group by authors.au_id 
+ORDER by sum(sales.qty) DESC 
+LIMIT 3; 
 
 
 
-SELECT pub_name from publishers,
-count(titles.title_id) as titles.published
-from publishers
-join titles
-on publishers.pub_id = titles.pub_id
-group by publishers.pub_name
-order by titles_published DESC ; 
+/*
+Challenge 4 - Best Selling Authors
+Now modify your solution in Challenge 3 so that the output will display 
+all 23 authors instead of the top 3. Note that the authors who have sold 
+0 titles should also appear in your output 
+(ideally display 0 instead of NULL as the TOTAL). 
+Also order your results based on TOTAL from high to low.
+Challenge 4 output
+*/
 
+SELECT 
+authors.au_id as 'AUTHOR ID', 
+authors.au_lname as 'LAST NAME', 
+authors.au_fname as 'FIRST NAME',
+COALESCE(sum(sales.qty),case when sales.qty ISNULL THEN 0 END) as 'TOTAL'
+from authors
+LEFT join titleauthor
+on authors.au_id = titleauthor.au_id
+LEFT join sales
+on sales.title_id = titleauthor.title_id
+group by authors.au_id 
+ORDER by sum(sales.qty) DESC
+limit 23; 
 
-
-SELECT * from titles; 
-
-
--- si quiero seleccionar solo las que han publicado 
-select DISTINCT publishers.pub_name from publishers
-join titles 
-on publishers.pub_id = titles.pub_id; 
-
--- Relaciones entre tableas 
--- inner join 
---- aquello que pertenece tanto a uno como a otro 
--- with 'primary key' par relacionar y con 'foreign key' - columna que hace referencia a clave primaria de otra tabla
--- quedarian filas 
--- list editorials and number titles per editorial 
-
-
-
-
-
-
-
-/* para comentar
--- para comentar
