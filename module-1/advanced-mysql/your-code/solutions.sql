5629b68c490a004c182718b3a0ad1c16be4fcce3
Marta <martaperezpuerta@gmail.com>
2020-05-27 18:07:21 +0200

    challenge 1 - step 2

--- module-1/advanced-mysql/your-code/solutions.sql
+++ module-1/advanced-mysql/your-code/solutions.sql
@@ -1,17 +1,38 @@
 Challenge 1
-
+Step 1 -
 select
-titles.title_id as 'Title ID',
-titleauthor.au_id as 'Author ID',
+titles.title_id as Title_ID,
+titleauthor.au_id as Author_ID,
 (cast(titleauthor.royaltyper as float) / 100 * titles.advance) as advance,
-(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as sales_royalty,
-titles.advance as 'REAL ADVANCE',
-titleauthor.royaltyper
+(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as sales_royalty
 from sales
 left join titles
 on  titles.title_id = sales.title_id
 left join titleauthor
 on titleauthor.title_id = sales.title_id
 group by sales.ord_num, titleauthor.au_id
-order by advance desc
+
+Step 2 -
+
+with royalties_sales_author as
+(
+select 
+titles.title_id as Title_ID,
+titleauthor.au_id as Author_ID,
+sales.ord_num,
+(cast(titleauthor.royaltyper as float) / 100 * titles.advance) as advance,
+(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as sales_royalty
+from sales
+left join titles
+on  titles.title_id = sales.title_id
+left join titleauthor 
+on titleauthor.title_id = sales.title_id
+group by sales.ord_num, titleauthor.au_id
+)
+SELECT
+Title_ID,
+Author_ID,
+advance + sales_royalty as profits
+from royalties_sales_author
+group by Title_ID, Author_ID
 
