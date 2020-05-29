8ddd56fa45638c90d36ed6232b436f828d0f7f16
ChristianJavierMelo <christian.javier.melo@gmail.com>
2020-05-29 16:03:41 +0200

    2nd challenge completed

--- module-1/advanced-mysql/your-code/solutions.sql
+++ module-1/advanced-mysql/your-code/solutions.sql
@@ -70,4 +70,33 @@ LIMIT 3;
 
 CHALLENGE 2: Alternative solution
 
+CREATE TEMPORARY TABLE IF NOT EXISTS sales_royalty as
+select 
+sales.title_id as Title_ID,
+authors.au_id as Author_ID,
+titles.advance*titleauthor.royaltyper/100 as advance,
+titles.price * sales.qty * titles.royalty/100 * titleauthor.royaltyper/100 as sales_royalty
+from titles
+join titleauthor on titleauthor.title_id = titles.title_id
+join authors on authors.au_id = titleauthor.au_id
+join sales on sales.title_id = titles.title_id
+ORDER BY author_ID DESC;
 
+CREATE TEMPORARY TABLE IF NOT EXISTS royalty_per_title_author as
+SELECT
+Title_ID,
+Author_ID,
+advance,
+sum(sales_royalty) as aggregated_royalties_per_title_author
+from sales_royalty 
+GROUP BY Author_ID;
+
+select
+Author_ID,
+aggregated_royalties_per_title_author + advance as total_proficts
+from royalty_per_title_author
+ORDER BY total_proficts DESC 
+LIMIT 3;
+
+
+CHALLENGE 3: 
