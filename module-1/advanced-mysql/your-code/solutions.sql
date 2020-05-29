516435a65d2888a1a58b70a4cf790a447879903f
Juan De Diego Suanzes <juandediegosuanzes@MacBook-Air-de-Juan.local>
2020-05-29 20:24:22 +0200

    lab-finished

--- module-1/advanced-mysql/your-code/solutions.sql
+++ module-1/advanced-mysql/your-code/solutions.sql
@@ -1,33 +1,148 @@
-CHALLENGE 1
+/*CHALLENGE 1
 
 STEP 1
-CALCULATE THE ROYALTY OF EACH SALE FOR EACH AUTHOR AND THE ADVANCE FOR EACH AUTHOR AND PUBLICATION 
---Calculate the royalty of each sale for each author and the advance for each author and publication
-Write a SELECT query to obtain the following output:
-
 1. Title ID
 2. Author ID
 3. Advance of each title and author
 4. Royalty of each sale
 
 
-SOLUTION QUERY
+SOLUTION QUERY*/
 select 
 titles.title_id  as TITLE_ID,
-authors.au_id AUTHOR_ID,
-(titles.advance * titleauthor.royaltyper) / 100 as ADVANCE,
---titleauthor.au_id,
---authors.au_fname,
---authors.au_lname,
+titleauthor.au_id AUTHOR_ID,
+(titles.advance * titleauthor.royaltyper/ 100) as ADVANCE,
 (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as SALES_ROYALTIES
 FROM 
-sales 
-join roysched 
-on sales.title_id = roysched.title_id 
-join titles 
-on roysched.title_id = titles.title_id
-join titleauthor 
-on titleauthor.title_id = titles.title_id
-join authors 
-on authors.au_id = titleauthor.au_id
-;
+titles
+JOIN titleauthor
+ON titles.title_id = titleauthor.title_id
+JOIN sales
+ON sales.title_id = titles.title_id;
+
+/*STEP2
+SOLUTION QUERY*/
+with table_step1 as
+(
+select 
+titles.title_id  as TITLE_ID,
+titleauthor.au_id AUTHOR_ID,
+(titles.advance * titleauthor.royaltyper/ 100) as ADVANCE,
+(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as SALES_ROYALTIES
+FROM 
+titles
+JOIN titleauthor
+ON titles.title_id = titleauthor.title_id
+JOIN sales
+ON sales.title_id = titles.title_id)
+select 
+titles.title_id  as TITLE_ID,
+titleauthor.au_id AUTHOR_ID,
+SUM(ADVANCE) AS "TOTAL_ADVANCE",
+sum(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as "AGGREGATE_ROYALTIES"
+FROM 
+titles
+JOIN titleauthor
+ON titles.title_id = titleauthor.title_id
+JOIN sales
+ON sales.title_id = titles.title_id
+GROUP BY titleauthor.au_id;
+
+/*STEP3
+SOLUTION QUERY*/
+with table_step2 as
+(
+select 
+titles.title_id  as TITLE_ID,
+titleauthor.au_id AUTHOR_ID,
+SUM(ADVANCE) AS "TOTAL_ADVANCE",
+sum(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as "AGGREGATE_ROYALTIES"
+--SUM(SALES_ROYALTIES) as "AGGREGATE_ROYALTIES"
+FROM 
+titles
+JOIN titleauthor
+ON titles.title_id = titleauthor.title_id
+JOIN sales
+ON sales.title_id = titles.title_id
+GROUP BY titleauthor.au_id)
+select
+titleauthor.au_id as AUTHOR_ID,
+SUM((titles.advance * titleauthor.royaltyper) / 100)
++
+sum(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS "PROFIT"
+FROM 
+titles
+JOIN titleauthor
+ON titles.title_id = titleauthor.title_id
+JOIN sales
+ON sales.title_id = titles.title_id
+GROUP BY AUTHOR_ID
+ORDER BY PROFIT DESC
+limit 3;
+
+
+--CHALLENGE 2
+
+--STEP 1
+--CREATE TEMPORARY TABLE
+CREATE TEMPORARY TABLE IF NOT EXISTS TEMP_TABLE_1 AS
+SELECT
+titles.title_id  as TITLE_ID,
+titleauthor.au_id AUTHOR_ID,
+(titles.advance * titleauthor.royaltyper/ 100) as ADVANCE,
+(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as SALES_ROYALTIES
+FROM 
+titles
+JOIN titleauthor
+ON titles.title_id = titleauthor.title_id
+JOIN sales
+ON sales.title_id = titles.title_id;
+
+--SELECT *
+--FROM TEMP_TABLE_1;
+
+--STEP 2
+--CREATE TEMPORARY TABLE
+CREATE TEMPORARY TABLE IF NOT EXISTS TEMP_TABLE_TOTALS AS
+select 
+titles.title_id  as TITLE_ID,
+titleauthor.au_id AUTHOR_ID,
+SUM(ADVANCE) AS "TOTAL_ADVANCE",
+sum(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as "AGGREGATE_ROYALTIES"
+FROM 
+titles
+JOIN titleauthor
+ON titles.title_id = titleauthor.title_id
+JOIN sales
+ON sales.title_id = titles.title_id
+GROUP BY titleauthor.au_id;
+
+--SELECT *
+--FROM TEMP_TABLE_TOTALS;
+
+
+--STEP 3
+--CREATE TEMPORARY TABLE
+CREATE TEMPORARY TABLE IF NOT EXISTS TEMP_TABLE_PROFIT AS
+select
+titleauthor.au_id as AUTHOR_ID,
+SUM((titles.advance * titleauthor.royaltyper) / 100)
++
+sum(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS "PROFIT"
+FROM 
+titles
+JOIN titleauthor
+ON titles.title_id = titleauthor.title_id
+JOIN sales
+ON sales.title_id = titles.title_id
+GROUP BY AUTHOR_ID
+ORDER BY PROFIT DESC
+limit 3;
+
+--SELECT *
+--FROM TEMP_TABLE_PROFIT;
+
+--CHALLENGE 3
+CREATE TABLE most_profiting_authors AS
+SELECT *
+FROM TEMP_TABLE_PROFIT;
