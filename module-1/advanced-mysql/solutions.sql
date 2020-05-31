d136c8d157c64aa47caa9d9003389b1e61d5dd04
Ignacio Sanchez Jurado <ignaciosanchezjurado@MacBook-Pro-de-Ignacio-2.local>
2020-05-31 23:38:33 +0200

    lab-started

---
+++ module-1/advanced-mysql/solutions.sql
@@ -0,0 +1,32 @@
+CHALLENGE 1
+
+WITH
+titleaut AS
+(
+SELECT *
+FROM titleauthor
+),
+titleauth_and_titles AS
+(
+SELECT *
+FROM titles
+LEFT JOIN titleaut
+ON titles.title_id = titleaut.title_id 
+),
+titleauth_and_titles_andsales AS
+(
+SELECT *
+FROM sales
+LEFT JOIN titleauth_and_titles
+ON sales.title_id = titleauth_and_titles.title_id 
+),
+royalty_and_advance AS
+(
+SELECT title_id, au_id, advance * royaltyper / 100 AS advance, price * qty * royalty / 100 * royaltyper / 100 AS sales_royalty
+FROM titleauth_and_titles_andsales
+)
+SELECT title_id AS "Title ID", au_id AS "Author ID", SUM(sales_royalty) AS "Aggregated royalties", SUM(advance) AS "Aggregated advance"
+FROM royalty_and_advance
+GROUP BY "Title ID", "Author ID"
+ORDER BY "Aggregated royalties" DESC
+;
