701a5bfab94bd59b14fc33a28a6ee7e2d6db031d
Marta <martaperezpuerta@gmail.com>
2020-06-01 01:17:26 +0200

    challenge 2

--- module-1/advanced-mysql/your-code/solutions.sql
+++ module-1/advanced-mysql/your-code/solutions.sql
@@ -4,7 +4,7 @@ with total_profits as
 (
 with royalties_sales_author as
 (
-select 
+select
 titles.title_id as Title_ID,
 titleauthor.au_id as Author_ID,
 sales.ord_num,
@@ -13,7 +13,7 @@ sales.ord_num,
 from titles
 join sales
 on sales.title_id = titles.title_id
-join titleauthor 
+join titleauthor
 on titleauthor.title_id = sales.title_id
 )
 select
@@ -30,4 +30,33 @@ order by profits desc
 limit 3
 
 CHALLENGE 2
+
+create temporary table if not exists top_profitable_authors as
+select
+Author_ID,
+profits
+from (
+select
+Author_ID,
+(advance + sales_royalty) as profits
+from (
+select
+titles.title_id as Title_ID,
+titleauthor.au_id as Author_ID,
+sales.ord_num,
+(titleauthor.royaltyper * titles.advance / 100) as advance,
+(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as sales_royalty
+from titles
+join sales
+on sales.title_id = titles.title_id
+join titleauthor
+on titleauthor.title_id = sales.title_id
+)
+group by Title_ID, Author_ID
+)
+order by profits desc
+limit 3
+
+CHALLENGE 3
+
 
