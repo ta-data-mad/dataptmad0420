bb5375f1cdf0285429b5f283c0eb6db85968329a
Sabine Goett <sabinegoett@hotmail.com>
2020-05-31 18:54:44 +0200

    lab-finished

--- module-1/advanced-mysql/solutions.sql
+++ module-1/advanced-mysql/solutions.sql
@@ -1,7 +1,7 @@
 Challenge 1
 Step1:
 
-select titleauthor.title_id as Title_ID, titleauthor.au_id as Author_ID, titles.advance*titleauthor.royaltyper /100 as advance,
+select titleauthor.title_id as "Title ID", titleauthor.au_id as "Author ID", titles.advance*titleauthor.royaltyper /100 as advance,
 titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper /100 as sales_royalty
 from titleauthor 
 left join titles 
@@ -11,9 +11,9 @@ on sales.title_id =titles.title_id;
 
 Step2:
 
-select Title_ID, Author_ID, advance, sum(sales_royalty) as total_royalties
+select "Title ID", "Author ID", advance, sum(sales_royalty) as total_royalties
 from (
-select titleauthor.title_id as Title_ID, titleauthor.au_id as Author_ID, titles.advance*titleauthor.royaltyper /100 as advance,
+select titleauthor.title_id as "Title ID", titleauthor.au_id as "Author ID", titles.advance*titleauthor.royaltyper /100 as advance,
 titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper /100 as sales_royalty
 from titleauthor 
 left join titles 
@@ -21,15 +21,15 @@ on titleauthor.title_id=titles.title_id
 left join sales 
 on sales.title_id =titles.title_id
 ) aggregation
-group by Author_ID, Title_ID;
+group by "Author ID", "Title ID";
 
 Step3:
 
-select Author_ID, sum(advance+total_royalties) as Total_profit
+select "Author ID", sum(advance+total_royalties) as Total_profit
 from (
-select Title_ID, Author_ID, advance, sum(sales_royalty) as total_royalties
+select "Title ID", "Author ID", advance, sum(sales_royalty) as total_royalties
 from (
-select titleauthor.title_id as Title_ID, titleauthor.au_id as Author_ID, titles.advance*titleauthor.royaltyper /100 as advance,
+select titleauthor.title_id as "Title ID", titleauthor.au_id as "Author ID", titles.advance*titleauthor.royaltyper /100 as advance,
 titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper /100 as sales_royalty
 from titleauthor 
 left join titles 
@@ -37,9 +37,9 @@ on titleauthor.title_id=titles.title_id
 left join sales 
 on sales.title_id =titles.title_id
 ) aggregation
-group by Author_ID, Title_ID
+group by "Author ID", "Title ID"
 ) aggregation2
-group by Author_ID
+group by "Author ID"
 order by Total_profit DESC 
 limit 3;
 
@@ -93,5 +93,6 @@ limit 3;
 
 select au_id, profits
 from most_profiting_authors;
+
 
 
