c372ff22e55c80f7d680c4cd42b4e717886351cb
Ignacio Sanchez Jurado <ignaciosanchezjurado@MacBook-Pro-de-Ignacio-2.local>
2020-06-01 12:41:13 +0200

    lab-inprogress

--- module-1/advanced-mysql/solutions.sql
+++ module-1/advanced-mysql/solutions.sql
@@ -24,9 +24,22 @@ royalty_and_advance AS
 (
 SELECT title_id, au_id, advance * royaltyper / 100 AS advance, price * qty * royalty / 100 * royaltyper / 100 AS sales_royalty
 FROM titleauth_and_titles_andsales
-)
+),
+royalty_and_advance_agreggated AS
+(
 SELECT title_id AS "Title ID", au_id AS "Author ID", SUM(sales_royalty) AS "Aggregated royalties", SUM(advance) AS "Aggregated advance"
 FROM royalty_and_advance
 GROUP BY "Title ID", "Author ID"
-ORDER BY "Aggregated royalties" DESC
+)
+SELECT "Title ID", "Author ID", "Aggregated royalties" + "Aggregated advance" AS "Profits"
+FROM royalty_and_advance_agreggated
+GROUP BY "Title ID", "Author ID"
+ORDER BY "Profits" DESC
+LIMIT 3
 ;
+
+
+
+CHALLENGE 2
+
+
