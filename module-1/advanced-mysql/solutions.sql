4bf16fd5ac01b5f89d29bbca5e5dd8b910f51cc2
Ignacio Sanchez Jurado <ignaciosanchezjurado@MacBook-Pro-de-Ignacio-2.local>
2020-06-01 19:33:23 +0200

    lab-finished

--- module-1/advanced-mysql/solutions.sql
+++ module-1/advanced-mysql/solutions.sql
@@ -29,4 +29,50 @@ LIMIT 3
 
 CHALLENGE 2
 
+CREATE TEMPORARY TABLE IF NOT EXISTS titleauth AS
+SELECT *
+FROM titles
+LEFT JOIN titleauthor ON titles.title_id = titleauthor.title_id 
+LEFT JOIN sales ON titles.title_id = sales.title_id;
 
+CREATE TEMPORARY TABLE IF NOT EXISTS royalty_and_advance AS
+SELECT title_id, au_id, advance * royaltyper / 100 AS advance, price * qty * royalty / 100 * royaltyper / 100 AS sales_royalty
+FROM titleauth;
+
+CREATE TEMPORARY TABLE IF NOT EXISTS royalty_and_advance_agreggated AS
+SELECT title_id AS "Title ID", au_id AS "Author ID", SUM(sales_royalty) AS "Aggregated royalties", SUM(advance) AS "Aggregated advance"
+FROM royalty_and_advance
+GROUP BY "Title ID", "Author ID";
+
+SELECT "Title ID", "Author ID", "Aggregated royalties" + "Aggregated advance" AS "Profits"
+FROM royalty_and_advance_agreggated
+GROUP BY "Title ID", "Author ID"
+ORDER BY "Profits" DESC
+LIMIT 3
+;
+
+
+
+
+CHALLENGE 3
+CREATE TEMPORARY TABLE IF NOT EXISTS titleauth AS
+SELECT *
+FROM titles
+LEFT JOIN titleauthor ON titles.title_id = titleauthor.title_id 
+LEFT JOIN sales ON titles.title_id = sales.title_id;
+
+CREATE TEMPORARY TABLE IF NOT EXISTS royalty_and_advance AS
+SELECT title_id, au_id, advance * royaltyper / 100 AS advance, price * qty * royalty / 100 * royaltyper / 100 AS sales_royalty
+FROM titleauth;
+
+CREATE TABLE IF NOT EXISTS royalty_and_advance_agreggated2 AS
+SELECT title_id AS "Title ID", au_id AS "Author ID", SUM(sales_royalty) AS "Aggregated royalties", SUM(advance) AS "Aggregated advance"
+FROM royalty_and_advance
+GROUP BY "Title ID", "Author ID";
+
+SELECT "Title ID", "Author ID", "Aggregated royalties" + "Aggregated advance" AS "Profits"
+FROM royalty_and_advance_agreggated
+GROUP BY "Title ID", "Author ID"
+ORDER BY "Profits" DESC
+LIMIT 3
+;
