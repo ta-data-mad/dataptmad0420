a7ab1b94492c50f45bf20bc460d117250cadce17
marodenas <marodenas6@gmail.com>
2020-06-10 20:04:55 +0200

    lab finished, reviewed after ta explanation

--- module-1/advanced-mysql/your-code/solutions.sql
+++ module-1/advanced-mysql/your-code/solutions.sql
@@ -7,9 +7,9 @@ titleauthor.title_id as "Title ID",
 (titles.advance * titleauthor.royaltyper / 100) as "Advance",
 (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as "Sales Royalty"
 FROM authors
-LEFT JOIN titleauthor on authors.au_id = titleauthor.au_id
-LEFT JOIN titles on titleauthor.title_id = titles.title_id 
-LEFT JOIN sales on sales.title_id = titles.title_id 
+JOIN titleauthor on authors.au_id = titleauthor.au_id
+JOIN titles on titleauthor.title_id = titles.title_id 
+JOIN sales on sales.title_id = titles.title_id 
 ),
 ---Step2---
 autores_royalty_advance as (
@@ -17,14 +17,14 @@ SELECT
 "Author ID",
 "Title ID",
 SUM("Sales Royalty") as "Aggregated royalties",
-SUM("Advance") as "Aggregated Advances"
+"Advance" as "Aggregated Advances"
 FROM autores
 GROUP BY "Author ID","Title ID"
 )
 ---Step3---
 SELECT 
 "Author ID",
-("Aggregated royalties"+"Aggregated Advances") as "Profits"
+SUM("Aggregated royalties"+"Aggregated Advances") as "Profits"
 FROM autores_royalty_advance 
 GROUP BY "AUTHOR id"
 ORDER BY "Profits" DESC
@@ -50,14 +50,14 @@ SELECT
 "Author ID",
 "Title ID",
 SUM("Sales Royalty") as "Aggregated royalties",
-SUM("Advance") as "Aggregated Advances"
+"Advance" as "Aggregated Advances"
 FROM autores
 GROUP BY "Author ID","Title ID"
 ;
 ---Step3---
 SELECT 
 "Author ID",
-("Aggregated royalties"+"Aggregated Advances") as "Profits"
+SUM("Aggregated royalties"+"Aggregated Advances") as "Profits"
 FROM autores_royalty_advance 
 GROUP BY "AUTHOR id"
 ORDER BY "Profits" DESC
@@ -69,9 +69,11 @@ LIMIT 3
 create table if not exists most_profiting_authors AS
 SELECT 
 "Author ID",
-("Aggregated royalties"+"Aggregated Advances") as "Profits"
+SUM("Aggregated royalties"+"Aggregated Advances") as "Profits"
 FROM autores_royalty_advance 
 GROUP BY "AUTHOR id"
 ORDER BY "Profits" DESC
 LIMIT 3
 ;
+
+
